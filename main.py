4a45681e8ca5fe1e979a06dafc0429c9104e20d7
Thang Hoang <thanghoang@Thangs-MacBook-Air.local>
2023-09-07 11:50:47 +0300

    modified main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QWidget,
     QFrame,
-    QSplitter
+    QSplitter,
+    QHBoxLayout
 )
 from PySide6.QtCore import (QSize,
                             Qt)
-# from PySide6.QtGui import()
+from src.views import HButton, PButton, SSlider, TSlider
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -32,7 +33,30 @@ class MainWindow(QMainWindow):
         # section header
         section_header = QFrame()
         section_header.setStyleSheet("background: red")
-        section_header.setFixedHeight(60)
+        section_header.setFixedHeight(50)
+        hlo_section_header = QHBoxLayout(section_header)
+
+        # tao HButton
+        btn_hbutton = HButton()
+        btn_hbutton.setText("Hai Button")
+        btn_hbutton.setStyleSheet('''
+    outline: 0px;
+    border: 0px;
+    margin: 0px;
+    vertical-align: middle;
+    text-decoration: none;
+    font-weight: 700;
+    line-height: 1.71429;
+    font-size: 0.875rem;
+    text-transform: unset;
+    font-family: "Public Sans", sans-serif;
+    min-width: 64px;
+    padding: 6px 12px;
+    border-radius: 8px;
+    background-color: rgb(0, 167, 111);
+                                    ''')
+        hlo_section_header.addWidget(btn_hbutton)
+
         # body
         section_body = QFrame()
         section_body.setStyleSheet("background: red")
