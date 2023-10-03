6c5fe03e1794d44571a4d1b8bb5eea59aed7b5f9
Hoang Minh Thang <mthang.hoang739@gmail.com>
2023-10-03 11:44:01 +0300

    minor changes

--- src/entities/palette/color.py
+++ src/entities/palette/color.py
@@ -1,13 +1,13 @@
 
 class PrimaryEntity():
     lighter: str = ""
-    lightstr = ""
+    light: str = ""
     main: str = ""
     dark: str = ""
     darker: str = ""
     contrastText: str = ""
 
-    def __init__(self, options: dict) -> None:
+    def __init__(self, options: dict):
         self.lighter = options["lighter"]
         self.light = options["light"]
         self.main = options["main"]
@@ -24,10 +24,11 @@ class SecondaryEntity():
     darker: str = ""
     contrastText: str = ""
 
-    def __init__(self, options: dict) -> None:
+    def __init__(self, options: dict):
         self.lighter = options["lighter"]
         self.light = options["light"]
         self.main = options["main"]
         self.dark = options["dark"]
         self.darker = options ["darker"]
         self.contrastText = options["contrastText"]
+
